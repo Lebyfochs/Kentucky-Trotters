b8691aebf71713bc0b555be38cd458e0bdb0baf8
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2025-06-18 19:01:04 +0100

    Makeshift movement
    Makeshift movement

--- KentuckyTrottersGJ/Assets/Scripts/PlayerMovement.cs
+++ KentuckyTrottersGJ/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float player_speed = 5.0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        
+    }
+
+    private void Awake()
     {
         
     }
@@ -11,6 +20,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, player_speed) * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -player_speed) * Time.deltaTime;
+
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(player_speed, 0) * Time.deltaTime;
+
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-player_speed, 0) * Time.deltaTime;
+
+        }
+
     }
 }
