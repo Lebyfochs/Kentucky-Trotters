c46f3a876db1393d75a32a4533b015692a91c9df
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2025-06-18 20:36:25 +0100

    basic lasso
    needs work

--- KentuckyTrottersGJ/Assets/Scripts/PlayerMovement.cs
+++ KentuckyTrottersGJ/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,16 @@ public class PlayerMovement : MonoBehaviour
 
     public float player_speed = 5.0f;
 
+    public GameObject lassoPrefab;
+
+    public Ray ray;
+
+    public Transform playerPoint;
+
+    public int lerpFramesCount = 45;
+    int elapsedFrames = 0;
+
+    //Vector3 mousePos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,28 +30,52 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float lerpRatio = (float)elapsedFrames / lerpFramesCount;
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0, player_speed) * Time.deltaTime;
         }
 
-        else if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.position += new Vector3(0, -player_speed) * Time.deltaTime;
 
         }
 
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(player_speed, 0) * Time.deltaTime;
 
         }
 
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(-player_speed, 0) * Time.deltaTime;
 
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Instantiate(lassoPrefab, new Vector3(playerPoint.position.x, playerPoint.position.y), Quaternion.identity);
+
+            //TO DO: Lerp the lasso towards the mouse position!
+
+            //lassoPrefab.transform.position = Vector3.Lerp(playerPoint.position, ray.origin,lerpRatio);
+            //elapsedFrames = (elapsedFrames + 1) % (lerpFramesCount + 1);
+
+            //lassoPrefab.transform.position += new Vector3(ray.origin.x, ray.origin.y) * Time.deltaTime;
+
+
+
+            //Instantiate(lassoPrefab, new Vector3(ray.origin.x, ray.origin.y), Quaternion.identity);
+        }
+
+        //Debug.DrawRay(transform.position, transform.forward, Color.cyan);
 
     }
 }
